b81a48eda683eb40c43ce96a16e84a20fef76bb7
Noam Amrani <noama@sela.co.il>
2024-01-28 09:31:30 +0200

    adding argocd

--- scripts/install_applications.sh
+++ scripts/install_applications.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+[ -d "$SCRIPT_DIR" ] || SCRIPT_DIR="."
 
 # Install the nginx application
 kubectl apply -f ${SCRIPT_DIR}/manifests/applications/nginx -n applications
+
+# Install ArgoCD
+helm upgrade -i argocd argo/argo-cd -n argo -f ${SCRIPT_DIR}/argocd/values.yaml
+kubectl apply -f ${SCRIPT_DIR}/manifests/applications/argocd -n argo
