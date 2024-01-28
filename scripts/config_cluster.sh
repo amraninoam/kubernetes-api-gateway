46d54241d626930d5e7f2a3d0046d37f03563d8a
Noam Amrani <noama@sela.co.il>
2024-01-28 09:33:45 +0200

    remove unneeded gaterway api installation

--- scripts/config_cluster.sh
+++ scripts/config_cluster.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+[ -d "$SCRIPT_DIR" ] || SCRIPT_DIR="."
 
 # Create relevant namespaces
 kubectl apply -f ${SCRIPT_DIR}/manifests/namespaces
 
 # Install the API Gateway CRDs
 # from: https://gateway-api.sigs.k8s.io/guides/#installing-gateway-api
-kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml
+# kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml
 
 # Install the Gateway
 kubectl apply -f ${SCRIPT_DIR}/manifests/administration
